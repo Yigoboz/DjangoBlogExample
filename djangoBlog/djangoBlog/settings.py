0ca6337ac18b64bc0d8688d86ee2adcd7443a24a
Yiğit Ozan BOZTEPE <yigoboz@gmail.com>
2026-07-10 23:40:25 +0300

    Core App Created

--- djangoBlog/djangoBlog/settings.py
+++ djangoBlog/djangoBlog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_blog.apps.DjangoBlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

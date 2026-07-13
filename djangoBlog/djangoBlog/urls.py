7b634db53473cf129469db3b50ebe7f796e71321
Yiğit Ozan BOZTEPE <yigoboz@gmail.com>
2026-07-13 18:01:03 +0300

    Services Page Settings Created

--- djangoBlog/djangoBlog/urls.py
+++ djangoBlog/djangoBlog/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('django_blog.urls'))
+    path('',include('django_blog.urls')),
+    path('services/',include('services.urls'))
 ]
--- djangoBlog/services/views.py
+++ djangoBlog/services/views.py
@@ -1,3 +1,10 @@
+from django.db.models import Model
 from django.shortcuts import render
+from .models import Service
 
 # Create your views here.
+
+def service_list(request):
+    services = Service.objects.all()
+
+    return render(request,'services/services.html',{'services':services})

2aada3d99d9e77802250595573251f07a312bd5c
Yiğit Ozan BOZTEPE <yigoboz@gmail.com>
2026-07-13 19:01:58 +0300

    Urls Redirections Maked For Service App

--- djangoBlog/django_blog/views.py
+++ djangoBlog/django_blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from services.models import Service
 
 # Create your views here.
 
 def index(request):
-    return render(request,template_name='django_blog/index.html')
+
+    featured = Service.objects.filter(is_featured=True)
+
+    return render(request,template_name='django_blog/index.html',context={'featured':featured})

c1a779a5094dfff06e1d7c4e3f0a7ae735b78232
Yiğit Ozan BOZTEPE <yigoboz@gmail.com>
2026-07-15 01:02:37 +0300

    feat: add paginaton to views.py in blog app

--- djangoBlog/blog/views.py
+++ djangoBlog/blog/views.py
@@ -1,13 +1,17 @@
 from django.db.models import Model
 from django.shortcuts import render, get_object_or_404
 from .models import Blog
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def post_list(request):
     posts = Blog.objects.all()
+    paginator = Paginator(posts,1)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
-    return render(request,'blog/post_list.html',{'posts':posts})
+    return render(request,'blog/post_list.html',{'posts':posts,'page_obj':page_obj})
 
 def post_details(request, slug):
     post_detail = get_object_or_404(Blog, slug=slug)

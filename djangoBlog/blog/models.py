bea64614c2e408023f1472d152b04ae641db9d00
Yiğit Ozan BOZTEPE <yigoboz@gmail.com>
2026-07-14 17:55:30 +0300

    feat: add author table to model

--- djangoBlog/blog/models.py
+++ djangoBlog/blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.db.backends.ddl_references import ForeignKeyName
 from django.utils.text import slugify
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -10,6 +12,7 @@ class Blog(models.Model):
     slug = models.SlugField(unique=True,blank=True)
     image = models.CharField(max_length=755,null=True,blank=True)
     created_at = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE())
 
     def __str__(self):
         return self.title

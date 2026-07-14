32aea1a5c83c2f7c77b1a14eb4f46566cfb6f3c0
Yiğit Ozan BOZTEPE <yigoboz@gmail.com>
2026-07-14 17:09:03 +0300

    Slug Skeleton Created

--- djangoBlog/services/views.py
+++ djangoBlog/services/views.py
@@ -1,5 +1,5 @@
 from django.db.models import Model
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Service
 
 # Create your views here.
@@ -8,3 +8,8 @@ def service_list(request):
     services = Service.objects.all()
 
     return render(request,'services/services.html',{'services':services})
+
+def service_detail(request, slug):
+    service = get_object_or_404(Service, slug=slug)
+
+    return render(request,template_name='services/service_detail.html',context={'service':service})

c966fb2a4b86366ad5bd59ad7ebfc982593dfb97
Yiğit Ozan BOZTEPE <yigoboz@gmail.com>
2026-07-14 18:01:52 +0300

    register Blog model to admin.py

--- djangoBlog/blog/admin.py
+++ djangoBlog/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Blog
 
 # Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title','slug','author')
+    search_fields = ('title', 'description','author')
+    list_filter = ('author')
+    row_id_fields = ['author']
+
+admin.site.register(Blog, BlogAdmin)

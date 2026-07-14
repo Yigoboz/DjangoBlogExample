93010e18f3447d4b7b939ea685389a2a5463dcb1
Yiğit Ozan BOZTEPE <yigoboz@gmail.com>
2026-07-15 00:46:44 +0300

    feat: add latest_models field in core-views.py

--- djangoBlog/django_blog/views.py
+++ djangoBlog/django_blog/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from services.models import Service
+from blog.models import Blog
 
 # Create your views here.
 
 def index(request):
 
     featured = Service.objects.filter(is_featured=True)
+    latest_models = Blog.objects.all()[:3]
 
-    return render(request,template_name='django_blog/index.html',context={'featured':featured})
+    return render(request,template_name='django_blog/index.html',context={'featured':featured,'latest_models':latest_models'})

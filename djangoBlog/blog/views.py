f49af18267ad2b3a1eaf35c0d248ae3fa87b9566
Yiğit Ozan BOZTEPE <yigoboz@gmail.com>
2026-07-15 00:12:16 +0300

    feat: create setup config to views.py and urls.py for blog app

--- djangoBlog/blog/views.py
+++ djangoBlog/blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.db.models import Model
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
 # Create your views here.
+
+def post_list(request):
+    posts = Blog.objects.all()
+
+    return render(request,'blog/post_list.html',{'posts':posts})
+
+def post_details(request, slug):
+    post_detail = get_object_or_404(Blog, slug=slug)
+
+    return render(request,template_name='blog/post_detail.html',context={'post':post_detail})

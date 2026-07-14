ab1b3aaa9e104adff67d2d295c899bc59cefb1ad
Yiğit Ozan BOZTEPE <yigoboz@gmail.com>
2026-07-14 17:43:43 +0300

    feat: write Blog model and make migrations

--- djangoBlog/blog/models.py
+++ djangoBlog/blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.text import slugify
+from django.utils import timezone
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    slug = models.SlugField(unique=True,blank=True)
+    image = models.CharField(max_length=755,null=True,blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+    def save(self,*args,**kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+
+        super(Blog,self).save(*args,**kwargs)
+
+    class Meta:
+        ordering = ['title']

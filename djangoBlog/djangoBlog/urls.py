e62e1e62a9fa08556aa687138b7d53550708e2ae
Yiğit Ozan BOZTEPE <yigoboz@gmail.com>
2026-07-14 23:57:23 +0300

    feat: change main urls.py

--- djangoBlog/djangoBlog/urls.py
+++ djangoBlog/djangoBlog/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('django_blog.urls')),
-    path('services/',include('services.urls'))
+    path('services/',include('services.urls')),
+    path('blog/',include('blog.urls'))
 ]
